c8071281acb3c4306859b2d21a4b4a444efcdaa7
EugeneMNS <evgen.str4@gmail.com>
2023-02-15 10:32:43 +0300

    scroll done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import UserItem from "./userList/UserItem";
+import { useInView } from 'react-intersection-observer';
+
 
 export type UserType = { name: string, surname: string };
 const arrUsers: UserType[] = [];
@@ -12,13 +14,25 @@ for (let i = 0; i < 100; i++) {
 function App() {
     const [offSet, setOffset] = useState(20)
 
+    const { ref, inView } = useInView({
+        threshold: 0.5,
+    });
+
+    useEffect(() => {
+        if (inView) {
+            setOffset(offSet + 20)
+        }
+    }, [inView]);
+
+
     return <>
         {arrUsers.map((user, index) => {
             if (index < offSet) {
                 return <UserItem user={user} index={index}/>
             }
         })}
-        // создай невидимый элемент, который будет отслеживать, когда он появится в зоне видимости
+        <div ref={ref}/>
+
 
     </>
 }
