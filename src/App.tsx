594aabc9980255fbbcc16fa29e0ddfed87a20c85
EugeneMNS <evgen.str4@gmail.com>
2023-02-15 11:28:42 +0300

    new array

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,18 @@ import { useInView } from 'react-intersection-observer';
 
 
 export type UserType = { name: string, surname: string };
-const arrUsers: UserType[] = [];
+/*const arrUsers: UserType[] = [];
 for (let i = 0; i < 100; i++) {
     arrUsers.push({name: `John`, surname: `Doe`});
-}
+}*/
 
+const names = ['John', 'Paul', 'George', 'Ringo'];
+const subNames = ['Sub', 'Sub', 'Sub', 'Sub'];
+const arrUsers_: Array<UserType & {id: number}> = [...Array(100)].map((_, index) => ({
+    id: index + 1,
+    name: names[index % names.length ],
+    surname: subNames[index % subNames.length ],
+}))
 
 function App() {
     const [offSet, setOffset] = useState(20)
@@ -26,7 +33,7 @@ function App() {
 
 
     return <>
-        {arrUsers.map((user, index) => {
+        {arrUsers_.map((user, index) => {
             if (index < offSet) {
                 return <UserItem user={user} index={index}/>
             }
